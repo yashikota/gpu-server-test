45fd140aee6e53c50ac0a24910c4ab1ecfd126d2
yashikota <52403688+yashikota@users.noreply.github.com>
2025-01-26 16:42:00 +0900

    change port

--- hello.py
+++ hello.py
@@ -72,4 +72,4 @@ app.router.add_get("/health", handle_health)
 app.router.add_post("/predict", handle_predict)
 
 if __name__ == "__main__":
-    web.run_app(app, host="0.0.0.0", port=8000)
+    web.run_app(app, host="0.0.0.0", port=8888)
